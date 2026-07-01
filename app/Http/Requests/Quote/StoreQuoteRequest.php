e6e3fb3e776981cb71a8478139c0dba06b36ce90
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2026-07-01 07:47:01 +0600

    Add quote form requests

---
+++ app/Http/Requests/Quote/StoreQuoteRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Quote;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+
+class StoreQuoteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'quote' => ['required', 'string', 'max:1000'],
+            'author' => ['required', 'string', 'max:255'],
+            'notes' => ['nullable', 'string', 'max:1000'],
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $normalized = [];
+
+        if ($this->filled('quote')) {
+            $normalized['quote'] = Str::squish((string) $this->input('quote'));
+        }
+
+        if ($this->filled('author')) {
+            $normalized['author'] = Str::squish((string) $this->input('author'));
+        }
+
+        if ($normalized !== []) {
+            $this->merge($normalized);
+        }
+    }
+}
